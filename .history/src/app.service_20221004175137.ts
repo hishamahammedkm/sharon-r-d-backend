cff762798c85ac658e9c2ab1d61550117580d73c
HISHAM AHAMMED K M <hisham.floges@gmail.com>
2022-10-04 17:53:30 +0530

    aws

---
+++ .history/src/app.service_20221004175137.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@nestjs/common';
+// import Client from '@veryfi/veryfi-sdk';
+const Client = require('@veryfi/veryfi-sdk');
+
+import fs from 'fs';
+import { AwsService } from './aws/aws.service';
+
+@Injectable()
+export class AppService {
+  constructor(private aws: AwsService) {}
+  getHello(): string {
+    return 'Hello World!';
+  }
+  getData = async (file) => {
+    // console.log('file 00000', file);
+   const data await this.aws.analyzeInvoice(file.filename);
+    const aws: { companyName?: string } = await this.aws.getData(file.filename);
+
+    // console.log('AWSSS---', aws);
+
+    const client_id = process.env.CLIENT_ID;
+    const client_secret = process.env.CLIENT_SECRET;
+    const username = process.env.USERNAME;
+    const api_key = process.env.API_KEY;
+    let sharonProducts = [];
+    let companyData = {};
+    let categories = ['Grocery', 'Utilities', 'Travel'];
+    const file_path = file.path;
+
+    // const file_path = './upload/invoice2.jpeg';
+
+    let veryfi_client = new Client(client_id, client_secret, username, api_key);
+    try {
+      let response = await veryfi_client.process_document(
+        file_path,
+        (categories = categories),
+      );
+      // fs.writeFile('jsonData.json', JSON.stringify(response), function (err) {
+      //   if (err) {
+      //     console.log(err);
+      //   }
+      // });
+      // console.log(response);
+      companyData['companyName'] =
+ 
+        aws['CompanyName'] || response.vendor.name;
+      companyData['gstNumber'] = aws['gstNumber'] || response.vat_number;
+      companyData['billNumber'] = aws['Bill Number'] || response.invoice_number;
+      response.line_items.forEach((product) => {
+        let position = product.description.search(/SHARON/i);
+        if (position >= 0) {
+          sharonProducts.push(product);
+        }
+      });
+      // console.log('sharonProducts---', sharonProducts);
+      return { sharonProducts, ...companyData };
+    } catch (error) {
+      console.log('error from varify api---', error);
+      return [];
+    }
+  };
+
+  processData = (data) => {
+    return data.reduce((prv, cur) => {
+      return prv + cur.price * cur.quantity;
+    }, 0);
+  };
+
+  async processFile(file: Express.Multer.File) {
+    const data = await this.getData(file);
+
+    let totoalPurchaseAmount = 0;
+    // @ts-ignore
+    if (data.sharonProducts) {
+      // @ts-ignore
+      totoalPurchaseAmount = this.processData(data.sharonProducts).toFixed(2);
+    }
+    console.log(totoalPurchaseAmount);
+    // console.log('data---', data);
+    // @ts-expect-error
+    if (data.length == 0) {
+      return {
+        statusCode: 404,
+        message: 'some this went wrong',
+      };
+    }
+
+    return { totoalPurchaseAmount, ...data };
+  }
+}
---
+++ .history/src/aws/aws.service_20221004105701.ts
@@ -0,0 +1,176 @@
+import { Injectable } from '@nestjs/common';
+// Import required AWS SDK clients and commands for Node.js
+import { AnalyzeDocumentCommand,AnalyzeExpenseCommand,Asd } from '@aws-sdk/client-textract';
+import { TextractClient } from '@aws-sdk/client-textract';
+import { textractHelper } from 'aws-textract-helper';
+import * as fs from 'fs';
+@Injectable()
+export class AwsService {
+  async getData(file) {
+    const filePath = `${__dirname}/upload/${file}`;
+    // console.log(`path from aws service--`, filePath);
+
+    // Set the AWS Region.
+    const REGION = 'us-east-1';
+    // Create SNS service object.
+    const textractClient = new TextractClient({ region: REGION });
+
+    // Set params
+    const params = {
+      Document: {
+        Bytes: fs.readFileSync(filePath),
+      },
+      FeatureTypes: ['TABLES', 'QUERIES'],
+      QueriesConfig: {
+        Queries: [
+          {
+            Alias: 'CompanyName',
+
+            Text: 'what is the company name',
+          },
+          {
+            Alias: 'Purchase amount',
+
+            Text: 'what is the total amount of SHARON',
+          },
+          {
+            Alias: 'Bill Number',
+
+            Text: 'what is invoice number',
+          },
+
+          {
+            Alias: 'gstNumber',
+            Text: 'what is the gst number',
+          },
+        ],
+      },
+    };
+
+    const displayBlockInfo = async (response) => {
+      let questions = [];
+      let answers = [];
+      let questionAndAnswers = {};
+      try {
+        response.Blocks.forEach((block) => {
+          if (block.BlockType === 'QUERY') {
+            if ('Relationships' in block && block.Relationships !== undefined) {
+              let answer = {
+                question: block.Query.Alias,
+                answerBolckId: block.Relationships[0].Ids[0],
+              };
+              questions.push(answer);
+            }
+          }
+          if (block.BlockType === 'QUERY_RESULT') {
+            answers.push({
+              id: block.Id,
+              text: block.Text,
+            });
+          }
+
+          //   console.log(`Block Type: ${block.BlockType}`);
+          //   if ("Text" in block && block.Text !== undefined) {
+          //     console.log(`Text: ${block.Text}`);
+          //   } else {
+          //   }
+          //   if ("Confidence" in block && block.Confidence !== undefined) {
+          //     console.log(`Confidence: ${block.Confidence}`);
+          //   } else {
+          //   }
+        });
+
+        questions.map((q) => {
+          answers.map((ans) => {
+            if (q.answerBolckId === ans.id) {
+              questionAndAnswers[q.question] = ans.text;
+            }
+          });
+        });
+      } catch (err) {
+        console.log('Error', err);
+      }
+      return questionAndAnswers;
+    };
+    const findRateAndQtyColumn = (obj) => {
+      let keys = Object.keys(obj);
+      let qtyKey;
+      let rateKey;
+      keys.map((key) => {
+        let item = obj[key];
+        let positionQty = item.search(/QTY/i);
+        if (positionQty >= 0) {
+          qtyKey = key;
+        }
+        let positionRate = item.search(/Rate/i);
+        if (positionRate >= 0) {
+          rateKey = key;
+        }
+      });
+      return {
+        qtyKey,
+        rateKey,
+      };
+    };
+    const getSharonProductTotalPurchaseAmount = (a) => {
+      let sharonItems = [];
+      let sharonItemsTotalAmount = 0;
+      // console.log("table----");
+      // console.log(a);
+      const rows = Object.values(a[0]);
+      const columnNames = rows[0];
+      let qtyAndRateKey = findRateAndQtyColumn(columnNames);
+      // console.log("-------qtyAndRateKey----", qtyAndRateKey);
+      // console.log(columnNames);
+      // console.log("-------");
+
+      // console.log("row---", rows);
+      rows.forEach((productData) => {
+        let values = Object.values(productData);
+        values.forEach((item) => {
+          let position = item.search(/SHARON/i);
+          if (position >= 0) {
+            sharonItems.push(productData);
+          }
+        });
+      });
+      sharonItems.forEach((item) => {
+        // console.log("itemmmm---");
+        // console.log(item);
+        // console.log("itemmmm---");
+        let qty = parseInt(item[qtyAndRateKey.qtyKey]);
+        let rate = parseFloat(item[qtyAndRateKey.rateKey]);
+        // console.log("rate-", rate, "qty-", qty);
+        if (typeof qty === 'number' && typeof rate === 'number') {
+          sharonItemsTotalAmount += rate * qty;
+        }
+      });
+      return sharonItemsTotalAmount.toFixed(2);
+    };
+
+    const analyze_document_text = async () => {
+      try {
+        const analyzeDoc = new AnalyzeDocumentCommand(params);
+        const response = await textractClient.send(analyzeDoc);
+        const questionAndAnswer = await displayBlockInfo(response);
+
+        // const tables = textractHelper.createTables(response);
+        // const totalPurchaseAmount = getSharonProductTotalPurchaseAmount(tables);
+        const responseData = {
+          ...questionAndAnswer,
+        };
+        return responseData;
+      } catch (err) {
+        console.log('Error', err);
+      }
+    };
+    const res = await analyze_document_text();
+    // console.log('data = +++++', res);
+    return res;
+  }
+
+  async analyzeInvoice (){
+
+
+  }
+}
